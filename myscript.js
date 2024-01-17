e294f81b4d13dbba2bb8873ae7ba20ceaca3d3d9
iTsThermo <117327847+iTsThermo@users.noreply.github.com.>
2024-01-17 00:42:39 -0500

    Created the round system for RPS

--- myscript.js
+++ myscript.js
@@ -1,13 +1,44 @@
+//Precondition: None
+//Poscondition: Returns random outcome - Rock, Paper, Scissors as computer choice
 function getComputerChoice(){
-    var choice = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-    if (choice == 0){
-        return "Rock";
-    }
-    if (choice == 1){
-        return "Paper";
-    }
-    if (choice == 2){
-        return "Scissors"
+    let choice = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    switch(choice){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
     }
 }
-console.log(getComputerChoice());
+
+const npc = getComputerChoice();
+console.log("NPC Choice: " + npc)
+const player = "Rock";
+
+function round(player, npc){
+    if (player == npc){
+        return "It was a tie, re-roll";
+    }
+    else if (player == 'Rock' && npc == 'Scissors'){
+        return "Player wins: Rock beats Scissors";
+    }
+    else if (player == 'Rock' && npc == 'Paper'){
+        return "Player loses: Rock loses against Paper";
+    }
+    else if (player == 'Paper' && npc == 'Scissors'){
+        return "Player Loses: Paper loses against Scissors";
+    }
+    else if(player == 'Paper' && npc == 'Rock'){
+        return "Player Loses: Paper wins against Rock";
+    }
+    else if(player == 'Scissors' && npc == 'Rock'){
+        return "Player Loses: Scissors loses against Rock";
+    }
+    else if(player == 'Scissors' && npc == 'Paper'){
+        return "Player Wins: Scissors wins against Paper";
+    }
+    
+}
+
+console.log(round(player, npc));
